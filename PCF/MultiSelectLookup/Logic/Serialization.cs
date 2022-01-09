f1c07f791cfa1bce018405bdaea0022ecd972c20
Aric Levin <33911954+ariclevin@users.noreply.github.com>
2022-01-08 23:48:29 -0500

    Update Serialization.cs
    Updated to include Update Plugin Serialization

--- PCF/MultiSelectLookup/Logic/Serialization.cs
+++ PCF/MultiSelectLookup/Logic/Serialization.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -37,15 +37,43 @@ namespace MDT.Plugins.MultiSelectLookup
         public string _relationshipType { get; set; }
         [DataMember] 
         public string _relationshipName { get; set; }
+        [DataMember] 
+        public LookupAttributeDetails _details { get; set; }
 
-        public LookupAttribute(string attributeName, string textAttributeName, string relationshipType, string relationshipName)
+        public LookupAttribute(string attributeName, string textAttributeName, string relationshipType, string relationshipName, LookupAttributeDetails details)
         {
             _attributeName = attributeName;
             _textAttributeName = textAttributeName;
             _relationshipType = relationshipType;
             _relationshipName = relationshipName;
-
+            _details = details;
         }
+    }
 
+    [DataContract]
+    public class LookupAttributeDetails
+    {
+        [DataMember] 
+        public string _primaryEntityName { get; set; }
+        [DataMember]
+        public string _relatedAttributeName { get; set; }
+        [DataMember] 
+        public string _linkToEntityName { get; set; }
+        [DataMember] 
+        public string _linkFromAttributeName { get; set; }
+        [DataMember] 
+        public string _linkToAttributeName { get; set; }
+        [DataMember] 
+        public string _linkToPrimaryAttributeName { get; set; }
+
+        public LookupAttributeDetails(string primaryEntityName, string relatedAttributeName, string linkToEntityName, string linkFromAttributeName, string linkToAttributeName, string linkToPrimaryAttributeName)
+        {
+            _primaryEntityName = primaryEntityName;
+            _relatedAttributeName = relatedAttributeName;
+            _linkToEntityName = linkToEntityName;
+            _linkFromAttributeName = linkFromAttributeName;
+            _linkToAttributeName = linkToAttributeName;
+            _linkToPrimaryAttributeName = linkToPrimaryAttributeName;
+        }
     }
 }
